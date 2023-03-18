18f7b6f1f026966d161dd472425ff5b5b656ecab
Andres Palacio <am.palaciol@uniandes.edu.co>
2023-03-17 20:40:13 -0500

    Update 3_NN.R

--- scripts/3_NN.R
+++ scripts/3_NN.R
@@ -4,3 +4,21 @@
 #                              4 - NEURAL ENTWORK
 #
 #------------------------------------------------------------------------------#
+
+install.packages('keras')
+library(keras)
+
+sum(is.na(train_clean_2$name))
+
+table(train_clean_2$name)
+
+Y <- as.factor(train_clean_2$name)
+Y <- dummy_cols(Y)
+Y <- as.matrix(Y)
+class(Y)
+head(Y)
+dim(Y)
+
+X <- as.matrix(tf_idf_reducido)
+class(X)
+dim(X)
