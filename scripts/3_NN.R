eb8cde3efd090d5e7b61dbcc85e892d4a2c82c81
Andres Palacio <am.palaciol@uniandes.edu.co>
2023-03-18 17:22:57 -0500

    Update 3_NN.R

--- scripts/3_NN.R
+++ scripts/3_NN.R
@@ -69,7 +69,7 @@ model %>%
 
 summary(model)
 
-model %>% compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = c('CategoricalAccuracy'))
+model %>% compile(optimizer = 'sgd', loss = 'mean_squared_error', metrics = c('accuracy'))
 
 history <- model %>% fit(X_train, y_train, epochs = 100, batch_size = 2^8, validation_split = 0.2)
 
@@ -86,5 +86,35 @@ y_hat <- model %>% predict(X_test) %>% k_argmax()
 confusionMatrix(data = factor(as.numeric(y_hat), levels = 1:3), 
                 reference = factor(train_clean_2$name2[-train_indices], levels = 1:3))
 
+
+
+
+##Modelo 2---------------------------------------------------------------------
+
+rm(model2)
+model2 <- keras_model_sequential() 
+model2 %>% 
+  layer_dense(units = 2, activation = 'relu', input_shape = ncol(X_train)) %>% 
+  layer_dropout(rate = 0.5) %>%
+  layer_dense(units = 4, activation = 'softmax')
+
+summary(model2)
+
+model2 %>% compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = c('accuracy'))
+
+history2 <- model2 %>% fit(X_train, y_train, epochs = 200, batch_size = 2^8, validation_split = 0.2)
+
+history_plot2 <- plot(history2)
+history_plot2
+
+model2 %>% evaluate(X_test, y_test)
+
+y_hat2 <- model2 %>% predict(X_test) %>% k_argmax()
+
+
+#library(caret)
+
+confusionMatrix(data = factor(as.numeric(y_hat2), levels = 1:3), 
+                reference = factor(train_clean_2$name2[-train_indices], levels = 1:3))
 
 
