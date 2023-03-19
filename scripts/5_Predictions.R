950697a6d7b33b4c99da6fe022d11918445a588e
Juan Sebastian Tellez Melo <sebastiantellezec@gmail.com>
2023-03-18 21:52:54 -0500

    Update 5_Predictions.R

--- scripts/5_Predictions.R
+++ scripts/5_Predictions.R
@@ -8,9 +8,9 @@
 #Prueba 1-----------------------------------------------------------------------
 
 x_prueba <- as.matrix(tf_idf_reducido2)
-y_hat_test <- model_1 %>% predict(x_prueba) %>% k_argmax()
+y_hat_test <- model_3 %>% predict(x_prueba) %>% k_argmax()
 
 resultados <- data.frame(id = test_final$id, name = factor(as.numeric(y_hat_test)))
 resultados$name <- ifelse(resultados$name == 1, "Lopez", ifelse(resultados$name == 2, "Petro", "Uribe"))
-write.table(resultados, "submission_4.csv", row.names = FALSE, quote=FALSE, sep = ",")
+write.table(resultados, "submission_5.csv", row.names = FALSE, quote=FALSE, sep = ",")
 
