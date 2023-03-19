3fe12b6d8a261ef893ab43c6a8ad2774f8cbfa4b
Juan Sebastian Tellez Melo <sebastiantellezec@gmail.com>
2023-03-18 21:28:58 -0500

    Update 5_Predictions.R

--- scripts/5_Predictions.R
+++ scripts/5_Predictions.R
@@ -7,29 +7,10 @@
 
 #Prueba 1-----------------------------------------------------------------------
 
-Z <- as.matrix(tf_idf_reducido2)
-class(Z)
-dim(Z)
-
-y_hat_test <- model3 %>% predict(Z) %>% k_argmax()
-
-predicho <- factor(as.numeric(y_hat_test))
-resultados <- data.frame(id = test_final$id, name = predicho)
-
-resultados$name <- ifelse(resultados$name == 1, "Lopez", ifelse(resultados$name == 2, "Petro", "Uribe"))
-resultados$name <- as.factor(resultados$name)
-write.table(resultados, "submission_1.csv", row.names = FALSE, quote=FALSE, sep = ",")
-
-
-<<<<<<< Updated upstream
-=======
-
-#Prueba 2-----------------------------------------------------------------------
-
 x_prueba <- as.matrix(tf_idf_reducido2)
 y_hat_test <- model_1 %>% predict(x_prueba) %>% k_argmax()
 
 resultados <- data.frame(id = test_final$id, name = factor(as.numeric(y_hat_test)))
 resultados$name <- ifelse(resultados$name == 1, "Lopez", ifelse(resultados$name == 2, "Petro", "Uribe"))
 write.table(resultados, "submission_4.csv", row.names = FALSE, quote=FALSE, sep = ",")
->>>>>>> Stashed changes
+
