3b63ba0a0d1ceeb765ba2cae5f3b452a2da6d4c8
Andres Palacio <am.palaciol@uniandes.edu.co>
2023-03-20 12:24:12 -0500

    Update 5_Predictions.R

--- scripts/5_Predictions.R
+++ scripts/5_Predictions.R
@@ -7,7 +7,7 @@
 
 x_prueba <- as.matrix(tf_idf_reducido2)
 dim(tf_idf_reducido2)
-y_hat_test <- model_12 %>% predict(x_prueba) %>% k_argmax()
+y_hat_test <- model_14 %>% predict(x_prueba) %>% k_argmax()
 
 resultados <- data.frame(id = test_final$id, name = factor(as.numeric(y_hat_test)))
 resultados$name <- ifelse(resultados$name == 1, "Lopez", ifelse(resultados$name == 2, "Petro", "Uribe"))
